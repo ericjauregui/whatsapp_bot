80cd4924307fb484c763f5e099c39cf648f9c40f
ERIC JAUREGUI <eric.jauregui@ymail.com>
2024-01-04 14:11:14 -0500

    wip rewriting to be based off selenium

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from pywhatkit.whats import sendwhatmsg_instantly, sendwhats_image
-from os import getcwd, listdir
-import check_file_paths as cp
-from time import sleep
-import pandas as pd
+# from pywhatkit.whats import sendwhatmsg_instantly, sendwhats_image
+# import logger # TODO set up logging once everything is final
+from os import listdir
 from sys import exit
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-PICS_PATH = getcwd() + '\\pictures'
-TARGETS_PATH = getcwd() + '\\recipients\\recipients.csv'
+import pandas as pd
+import utils
+
+BASE_URL = "https://web.whatsapp.com/send?"
+PICS_PATH = "../pictures/"
+TARGETS_PATH = "../recipients/recipients.csv"
+
+# Initialize the Chrome Driver with local cookies for verification
+options = webdriver.ChromeOptions()
+options.add_argument(
+    "user-data-dir=/Users/ericj/Library/Application Support/Google/Chrome/Profile 1"
+)
+driver = webdriver.Chrome(chrome_options=options)
+wait = WebDriverWait(driver, 100)
+
+driver.get("https://web.whatsapp.com/send?phone=573102573010")
+
+message_box_path = """//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"""
+message_box = wait.until(
+    EC.presence_of_element_located((By.XPATH, message_box_path))
+)
+input = "hello i am a robot"
+message_box.click()
+message_box.send_keys(input, Keys.ENTER)
+# TODO how to copy paste picture based on path provided
+# TODO write functions for sending messages and pictures
 
 
 def mass_send_msg(
@@ -24,7 +52,7 @@ def mass_send_msg(
         3. Have all contacts filled in the recipients.csv file
     """
     df = pd.read_csv(recipients, header=0)
-    df['phone_number'] = df['phone_number'].apply(str).apply(lambda x: '+' + x)
+    df["phone_number"] = df["phone_number"].apply(str).apply(lambda x: "+" + x)
     n = [x for x in range(len(df))]
     # gen = ((df['first_name'][i], df['phone_number'][i]) for i in n)
 
@@ -33,8 +61,8 @@ def mass_send_msg(
         for i in n:
             for img in imgs:
                 sendwhats_image(
-                    receiver=df['phone_number'][i],
-                    img_path=PICS_PATH + '\\' + img,
+                    receiver=df["phone_number"][i],
+                    img_path=PICS_PATH + "\\" + img,
                     caption=message,
                     wait_time=wait_time,
                     tab_close=tab_close,
@@ -44,49 +72,58 @@ def mass_send_msg(
     else:
         for i in n:
             sendwhatmsg_instantly(
-                phone_no=df['phone_number'][i],
+                phone_no=df["phone_number"][i],
                 message=message,
                 wait_time=wait_time,
                 tab_close=tab_close,
                 close_time=close_time,
             )
             sleep((wait_time - 7) + close_time)
-    return f'{len(df)} messages sent successfully!'
+    return f"{len(df)} messages sent successfully!"
 
 
-cp.check_dir()
+utils.check_dir()
 
-print("""
+print(
+    """
       1. Please ensure you're signed into whatsapp web on this computer
       2. Have pictures you want to send in the pictures folder
       3. Ensure that all contacts are filled in the recipients.csv file
       4. Don't touch your computer while this is running!
       5. You can stop this program by pressing ctrl+c
-      """)
+      """
+)
 
 df = pd.read_csv(TARGETS_PATH, header=0)
 n = [x for x in range(len(df))]
 
-confirm_recipients = input(f"""
+confirm_recipients = input(
+    f"""
         Are you sure you want to send messages to {len(n)} recipients?
-                           """).lower()
-if confirm_recipients == 'yes':
+                           """
+).lower()
+if confirm_recipients == "yes":
     pics_flow = input(
         """Type yes if you want to send pictures in the 'pictures' folder along with the message:
-            """).lower()
-    if pics_flow == 'yes':
-        print(mass_send_msg(
-            recipients=TARGETS_PATH,
-            message=input("Enter your message: "),
-            tab_close=True,
-            include_pics=True,
-        ))
+            """
+    ).lower()
+    if pics_flow == "yes":
+        print(
+            mass_send_msg(
+                recipients=TARGETS_PATH,
+                message=input("Enter your message: "),
+                tab_close=True,
+                include_pics=True,
+            )
+        )
     else:
-        print(mass_send_msg(
-            recipients=TARGETS_PATH,
-            message=input("Enter your message: "),
-            tab_close=True,
-            include_pics=False,
-        ))
+        print(
+            mass_send_msg(
+                recipients=TARGETS_PATH,
+                message=input("Enter your message: "),
+                tab_close=True,
+                include_pics=False,
+            )
+        )
 else:
     exit("Please try again and type 'yes' if you want to start the program")
