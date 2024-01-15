0e7e13a9c666ccbf9b4268ed3ceef7cdbfaa48ea
ERIC JAUREGUI <eric.jauregui@ymail.com>
2024-01-14 19:20:25 -0500

    finalized working version of whatsapp bot. need to set up logging still

--- main.py
+++ main.py
@@ -1,111 +1,45 @@
 # import logger # TODO set up logging once everything is final
-# TODO rewrite readme generated off copilot at end
-# from os import listdir, path
 from utils import utils
-
-# from sys import exit
+from csv import reader
+from sys import exit
 
 COOKIES = "/Users/ericj/Library/Application Support/Google/Chrome/Profile 1"
 
 if utils.check_wd():
-    for i in range(5):
-        utils.send_message(
-            cookies_path=COOKIES,
-            include_pics=True,
-            message="Hello, I am Marco the robot",
-            wait_time=5,
-            process_timeout=15,
-        )
-
-
-# def mass_send_msg(
-#     recipients: str = TARGETS_PATH,
-#     message: str = "",
-#     wait_time: int = 8,
-#     tab_close: bool = True,
-#     close_time: int = 3,
-#     include_pics: bool = True,
-# ) -> str:
-#     """Send Pictures or Messages to Multiple WhatsApp Contacts
-#     Requirements:
-#         1. Sign in whatsapp web
-#         2. Have pictures you want to send in the pictures folder
-#         3. Have all contacts filled in the recipients.csv file
-#     """
-#     if utils.check_wd():
-#         df = pd.read_csv(recipients, header=0)
-#         df["phone_number"] = (
-#             df["phone_number"].apply(str).apply(lambda x: "+" + x)
-#         )
-#         n = [x for x in range(len(df))]
-# gen = ((df['first_name'][i], df['phone_number'][i]) for i in n)
-
-# if include_pics:
-#     imgs = [x for x in listdir(PICS_PATH)]
-#     for i in n:
-#         for img in imgs:
-#             # sendwhats_image(
-#             #     receiver=df["phone_number"][i],
-#             #     img_path=PICS_PATH + "\\" + img,
-#             #     caption=message,
-#             #     wait_time=wait_time,
-#             #     tab_close=tab_close,
-#             #     close_time=close_time,
-#             # )
-#             # sleep((wait_time - 7) + close_time)
-# else:
-#     for i in n:
-#         sendwhatmsg_instantly(
-#             phone_no=df["phone_number"][i],
-#             message=message,
-#             wait_time=wait_time,
-#             tab_close=tab_close,
-#             close_time=close_time,
-#         )
-#         sleep((wait_time - 7) + close_time)
-# return f"{len(df)} messages sent successfully!"
-
-
-# print(
-#     """
-#       1. Please ensure you're signed into whatsapp web on this computer
-#       2. Have pictures you want to send in the pictures folder
-#       3. Ensure that all contacts are filled in the recipients.csv file
-#       4. Don't touch your computer while this is running!
-#       5. You can stop this program by pressing ctrl+c
-#       """
-# )
-
-# df = pd.read_csv(TARGETS_PATH, header=0)
-# n = [x for x in range(len(df))]
-
-# confirm_recipients = input(
-#     f"""
-#         Are you sure you want to send messages to {len(n)} recipients?
-#                            """
-# ).lower()
-# if confirm_recipients == "yes":
-#     pics_flow = input(
-#         """Type yes if you want to send pictures in the 'pictures' folder along with the message:
-#             """
-#     ).lower()
-#     if pics_flow == "yes":
-#         print(
-#             mass_send_msg(
-#                 recipients=TARGETS_PATH,
-#                 message=input("Enter your message: "),
-#                 tab_close=True,
-#                 include_pics=True,
-#             )
-#         )
-#     else:
-#         print(
-#             mass_send_msg(
-#                 recipients=TARGETS_PATH,
-#                 message=input("Enter your message: "),
-#                 tab_close=True,
-#                 include_pics=False,
-#             )
-#         )
-# else:
-#     exit("Please try again and type 'yes' if you want to start the program")
+    print(
+        """
+          1. Please ensure you're signed into whatsapp web on this computer
+          2. Have pictures you want to send in the pictures folder
+          3. Ensure that all contacts are filled in the recipients.csv file
+          4. Don't touch your computer while this is running!
+          5. You can stop this program by pressing ctrl+c
+          """
+    )
+    with open("recipients/recipients.csv", "r") as file:
+        csv = reader(file)
+        recipients = list(map(lambda x: x[2], list(csv)))[1:]
+    confirm_recipients = input(
+        f"Are you sure you want to send messages to {len(recipients)} recipients?\n"
+    ).lower()
+    if confirm_recipients == "yes":
+        pics_flow = input(
+            "Type yes if you want to send pictures in the 'pictures' folder along with the message:\n"
+        ).lower()
+        if pics_flow == "yes":
+            utils.send_message(
+                cookies_path=COOKIES,
+                include_pics=True,
+                message=input("Enter your message: "),
+                wait_time=5,
+                process_timeout=15,
+            )
+        else:
+            utils.send_message(
+                cookies_path=COOKIES,
+                include_pics=False,
+                message=input("Enter your message: "),
+                wait_time=5,
+                process_timeout=15,
+            )
+    else:
+        exit("Please try again and type 'yes' to start the program")
--- utils/utils.py
+++ utils/utils.py
@@ -4,11 +4,9 @@ def clean_number(number: str) -> str:
     else:
         return number
 
-
 def open_page(driver, base_url: str, receiver: str):
     url = f"{base_url}phone={clean_number(receiver)}"
     return driver.get(url)
-
 
 def check_wd() -> bool:
     from csv import writer
@@ -23,76 +21,6 @@ def check_wd() -> bool:
             writer = writer(f)
             writer.writerow(["first_name", "last_name", "phone_number"])
     return True
-
-
-def send_message(
-    cookies_path: str,
-    base_url: str = "https://web.whatsapp.com/send?",
-    receivers_path: str = "recipients/recipients.csv",
-    include_pics: bool = False,
-    message: str = " ",
-    wait_time: int = 3,
-    process_timeout: int = 15,
-) -> str:
-    from time import sleep
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from utils import utils
-    from csv import reader
-    from os import listdir
-    from pyautogui import hotkey
-    from platform import system as sys
-
-    with open(receivers_path, "r") as file:
-        csv = reader(file)
-        receivers = list(map(lambda x: x[2], list(csv)))[1:]
-
-    # initialize webdriver args
-    chrome_options = Options()
-    chrome_options.add_argument(f"--user-data-dir={cookies_path}")
-    txt_xpath = """//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"""
-    send_pic_xpath = '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span'
-    driver = webdriver.Chrome(options=chrome_options)
-    for phone in receivers:
-        wait = WebDriverWait(driver, process_timeout)
-        utils.open_page(driver=driver, base_url=base_url, receiver=phone)
-        sleep(wait_time)
-        txt_box = wait.until(
-            EC.presence_of_element_located((By.XPATH, txt_xpath))
-        )
-        txt_box.click()
-        txt_box.send_keys(message, Keys.ENTER)
-        if include_pics:
-            for pic in listdir("pictures"):
-                utils.copy_image(f"pictures/{pic}")
-                pic_txt_box = wait.until(
-                    EC.presence_of_element_located((By.XPATH, txt_xpath))
-                )
-                pic_txt_box.click()
-                utils.paste_image(pic_txt_box)
-                sleep(wait_time)
-                send_pic = wait.until(
-                    EC.presence_of_element_located((By.XPATH, send_pic_xpath))
-                )
-                send_pic.click()
-                sleep(wait_time)
-        if len(receivers) > 1:
-            _system = sys().lower()
-            if _system == "windows":
-                hotkey("ctrl", "t")
-                sleep(1)
-                hotkey("ctrl", "w")
-            elif _system == "darwin":
-                hotkey("command", "t")
-                sleep(1)
-                hotkey("command", "w")
-        # TODO figure out how to close & reopen tabs without errors
-    return f"{len(receivers)} messages sent successfully!"
-
 
 def copy_image(relative_path: str) -> None:
     """Copy the Image to Clipboard based on the Platform"""
@@ -151,3 +79,86 @@ def paste_image(input_field) -> None:
     else:
         raise Exception(f"Unsupported System: {_system}")
     return None
+
+def send_message(
+    cookies_path: str,
+    base_url: str = "https://web.whatsapp.com/send?",
+    receivers_path: str = "recipients/recipients.csv",
+    include_pics: bool = False,
+    message: str = " ",
+    wait_time: int = 3,
+    process_timeout: int = 30,
+) -> str:
+    """Send Pictures or Messages to Multiple WhatsApp Contacts
+    Requirements:
+        1. Sign in whatsapp web
+        2. Have pictures you want to send in the pictures folder
+        3. Have all contacts filled in the recipients.csv file
+    """
+    from time import sleep
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from csv import reader
+    from os import listdir
+    from pyautogui import hotkey
+    from platform import system as sys
+
+    with open(receivers_path, "r") as file:
+        csv = reader(file)
+        receivers = list(map(lambda x: x[2], list(csv)))[1:]
+
+    # initialize webdriver args
+    chrome_options = Options()
+    chrome_options.add_argument(f"--user-data-dir={cookies_path}")
+    txt_xpath = """//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p"""
+    send_pic_xpath = '//*[@id="app"]/div/div[2]/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div/span'
+    driver = webdriver.Chrome(options=chrome_options)
+    for phone in receivers:
+        wait = WebDriverWait(driver, process_timeout)
+        open_page(driver=driver, base_url=base_url, receiver=phone)
+        sleep(wait_time)
+        txt_box = wait.until(
+            EC.presence_of_element_located((By.XPATH, txt_xpath))
+        )
+        txt_box.click()
+        txt_box.send_keys(message, Keys.ENTER)
+        print(f"{'\'' + message + '\''} sent successfully to {phone}!")
+        if include_pics:
+            for pic in listdir("pictures"):
+                copy_image(f"pictures/{pic}")
+                pic_txt_box = wait.until(
+                    EC.presence_of_element_located((By.XPATH, txt_xpath))
+                )
+                pic_txt_box.click()
+                paste_image(pic_txt_box)
+                sleep(wait_time)
+                send_pic = wait.until(
+                    EC.presence_of_element_located((By.XPATH, send_pic_xpath))
+                )
+                send_pic.click()
+                sleep(wait_time)
+        print(f"{len(listdir("pictures"))} pictures sent successfully to {phone}!")
+        if len(receivers) > 1:
+            import pyautogui as gui
+            _system = sys().lower()
+            if _system == "windows":
+                size = gui.size()
+                gui.moveTo(size[0] / 2, size[1] / 2)
+                gui.click()
+                sleep(1)
+                hotkey("ctrl", "t")
+                sleep(1)
+                hotkey("ctrl", "w")
+            elif _system == "darwin":
+                size = gui.size()
+                gui.moveTo(size[0] / 2, size[1] / 2)
+                gui.click()
+                sleep(1)
+                hotkey("command", "t")
+                sleep(1)
+                hotkey("command", "w")
+    return f"{len(receivers)} messages sent successfully!"
